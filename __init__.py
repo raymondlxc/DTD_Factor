2a1c3f412e5cfacbfee3bbedd4ef237202b8f0d2
veyjane <379124677@qq.com>
2018-10-15 23:02:48 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -36,29 +36,35 @@ class Stock():
         sql="SELECT R FROM interestrate WHERE reportdate='%s'"%EndDate
         cur.execute(sql)
         results = cur.fetchone()
-        return results[0]
+        return results
 
     def getF(self):#获取报告日期的上市公司债务
         sql="SELECT totalliab FROM financial WHERE secid='%s' and reportdate='%s'"%(secid,EndDate)
         cur.execute(sql)
         results = cur.fetchone()
-        return results[0]
+        return results
 
-    def getSt(self):#获取报告日期的上市公司市值
+    def getVt(self):#获取报告日期的上市公司市值
         sql="SELECT totalassets FROM financial WHERE secid='%s' and reportdate='%s'"%(secid,EndDate)
         cur.execute(sql)
         results = cur.fetchone()
-        return results[0]
+        return results
 
+    def getSt(self):#获取报告日期的上市公司的股价
+        delta = relativedelta(years=0,months=0,days=0)
+        sql="SELECT closeprice FROM stockprice WHERE secid='%s' and tradedate='%s'"%(secid,EndDate)
+        cur.execute(sql)
+        results = cur.fetchone()
+        return results
 
-    def get_sigma_St(self):#获取上市公司报告期前一年的股价收益波动率
+    def get_sigma_St(self,EndDate):#获取上市公司报告期前一年的股价收益波动率
         delta = relativedelta(years=-1,months=0,days=0)
-        RangeStartDate=datetime.datetime.strptime(EndDate,'%Y%m%d')+delta #获取一年区间的起始日期
+        RangeStartDate=EndDate+delta #获取一年区间的起始日期
         sql="SELECT closeprice FROM stockprice WHERE secid='%s' and tradedate >='%s' and tradedate<='%s' and volume!=0"%(secid,RangeStartDate,EndDate)
         df=pd.read_sql(sql,conn)#将查询结果转化为dataframe格式
         data=df['closeprice'].tolist()#将dataframe转为list
         logreturns = diff(log(data))
-        sigma_St= np.std(logreturns)*sqrt(250)
+        sigma_St= np.std(logreturns)
         return sigma_St
 
 def main():
@@ -73,18 +79,18 @@ def main():
     cur = conn.cursor()
     Stocklist=getAStock()
     for secid in Stocklist:
+        secid=secid[0]
+        secid='000001.SZ'
         StartDate=getIpodate(secid)
-        if StartDate<'20020131':#数据有效日期从2002年1月31日开始
-            StartDate='20020131'
         TodayDate=getCurrentTime()
         Datelist=get_date_list(StartDate,TodayDate)
         for EndDate in Datelist:
             stock=Stock(secid,EndDate)
-            St=stock.getSt()/1000000
-            sigma_St=stock.get_sigma_St()
-            F=stock.getF()/1000000
-            EV=F+St#资产价值为账面市值加负债，作为方程的初值求解
-            r=stock.getRate()/100
+            EV=stock.getVt()
+            F=stock.getF()
+            r=stock.getRate()
+            St=stock.getSt()
+            sigma_St=stock.get_sigma_St(EndDate)
             DTD=KMV(EV,St,sigma_St,F,r)
 
 
