00d3798a215c2ddc75fb1ecc2dd292f164f77fdb
veyjane <379124677@qq.com>
2018-10-16 10:16:28 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -36,35 +36,29 @@ class Stock():
         sql="SELECT R FROM interestrate WHERE reportdate='%s'"%EndDate
         cur.execute(sql)
         results = cur.fetchone()
-        return results
+        return results[0]
 
     def getF(self):#获取报告日期的上市公司债务
         sql="SELECT totalliab FROM financial WHERE secid='%s' and reportdate='%s'"%(secid,EndDate)
         cur.execute(sql)
         results = cur.fetchone()
-        return results
+        return results[0]
 
-    def getVt(self):#获取报告日期的上市公司市值
+    def getSt(self):#获取报告日期的上市公司市值
         sql="SELECT totalassets FROM financial WHERE secid='%s' and reportdate='%s'"%(secid,EndDate)
         cur.execute(sql)
         results = cur.fetchone()
-        return results
+        return results[0]
 
-    def getSt(self):#获取报告日期的上市公司的股价
-        delta = relativedelta(years=0,months=0,days=0)
-        sql="SELECT closeprice FROM stockprice WHERE secid='%s' and tradedate='%s'"%(secid,EndDate)
-        cur.execute(sql)
-        results = cur.fetchone()
-        return results
 
-    def get_sigma_St(self,EndDate):#获取上市公司报告期前一年的股价收益波动率
+    def get_sigma_St(self):#获取上市公司报告期前一年的股价收益波动率
         delta = relativedelta(years=-1,months=0,days=0)
-        RangeStartDate=EndDate+delta #获取一年区间的起始日期
+        RangeStartDate=datetime.datetime.strptime(EndDate,'%Y%m%d')+delta #获取一年区间的起始日期
         sql="SELECT closeprice FROM stockprice WHERE secid='%s' and tradedate >='%s' and tradedate<='%s' and volume!=0"%(secid,RangeStartDate,EndDate)
         df=pd.read_sql(sql,conn)#将查询结果转化为dataframe格式
         data=df['closeprice'].tolist()#将dataframe转为list
         logreturns = diff(log(data))
-        sigma_St= np.std(logreturns)
+        sigma_St= np.std(logreturns)*sqrt(250)
         return sigma_St
 
 def main():
@@ -77,22 +71,65 @@ def main():
         db='testdb',
         )
     cur = conn.cursor()
+    #创建数据库表
+    cur.execute("DROP TABLE IF EXISTS Results")
+    cur.execute("""
+        CREATE TABLE Results(
+        secid VARCHAR(20) NOT NULL,
+        EndDate VARCHAR(20),
+        St FLOAT,
+        sigma_St FLOAT,
+        F FLOAT,
+        Vt_ini FLOAT,
+        r FLOAT,
+        Vt FLOAT,
+        sigma FLOAT,
+        dt FLOAT,
+        status VARCHAR(20)
+        )""");
     Stocklist=getAStock()
     for secid in Stocklist:
-        secid=secid[0]
-        secid='000001.SZ'
+        secid=secid[0] #读取元组中的数据
         StartDate=getIpodate(secid)
+        if StartDate<'20020131':#数据有效日期从2002年1月31日开始
+            StartDate='20020131'
         TodayDate=getCurrentTime()
         Datelist=get_date_list(StartDate,TodayDate)
         for EndDate in Datelist:
+            EndDate=EndDate.strftime('%Y%m%d')
             stock=Stock(secid,EndDate)
-            EV=stock.getVt()
-            F=stock.getF()
-            r=stock.getRate()
-            St=stock.getSt()
-            sigma_St=stock.get_sigma_St(EndDate)
-            DTD=KMV(EV,St,sigma_St,F,r)
-
+            St=stock.getSt()/1000000000
+            sigma_St=stock.get_sigma_St()
+            F=stock.getF()/1000000000
+            Vt_ini=F+St#资产价值为账面市值加负债，作为方程的初值求解
+            r=stock.getRate()/100
+            try:
+                DTD=KMV(Vt_ini,St,sigma_St,F,r)
+            except ValueError:
+                status='fail'#错误类型为传入参数初值有误
+                print(secid+'日期'+EndDate+'DTD计算失败')
+                sqltuple=(secid,EndDate,St,sigma_St,F,Vt_ini,r,0,0,0,status)
+                cur.execute("INSERT INTO Results VALUES('%s','%s','%f','%f','%f','%f','%f','%f','%f','%f','%s')"%sqltuple)
+            except Exception:
+                status='Runtime'#以状态字判断迭代超时
+                print('正在计算'+secid+'日期'+EndDate+'DTD:迭代超时')
+                print(DTD)
+                Vt=DTD[0]
+                sigma=DTD[1]
+                dt=DTD[2]
+                sqltuple=(secid,EndDate,St,sigma_St,F,Vt_ini,r,Vt,sigma,dt,status)
+                cur.execute("INSERT INTO Results VALUES('%s','%s','%f','%f','%f','%f','%f','%f','%f','%f','%s')"%sqltuple)
+            else:
+                status='success'#以状态字判断迭代成功
+                print('正在计算'+secid+'日期'+EndDate+'DTD:成功')
+                print(DTD)
+                Vt=DTD[0]
+                sigma=DTD[1]                 
+                dt=DTD[2]
+                sqltuple=(secid,EndDate,St,sigma_St,F,Vt_ini,r,Vt,sigma,dt,status)
+                cur.execute("INSERT INTO Results VALUES('%s','%s','%f','%f','%f','%f','%f','%f','%f','%f','%s')"%sqltuple)
+            conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
     main()
---
+++ solve.py
@@ -0,0 +1,29 @@
+from __future__ import division
+from scipy.optimize import fsolve,leastsq
+from scipy.stats import norm
+from math import pow, e, log
+import numpy as np
+import pandas as pd
+def KMV(EV,St,sigma_St,F,r):
+
+    def f(x):
+        #定义fsolve函数所需要的参数f
+
+        Vt = float(x[0])
+        sigma = float(x[1])
+        dt = float(x[2])
+
+        def N(y):
+    #   定义标准正态分布的累积函数
+            return norm.cdf(y)
+
+        return [Vt*N(dt) - pow(e, -r)*F*N(dt - sigma) - St,
+                sigma*Vt*N(dt)/St - sigma_St,
+                (log(Vt/F) + r + pow(sigma, 2)/2)- sigma*dt]
+    x0 = [EV,0.5,0.5]
+    result = fsolve(f,x0,epsfcn=0.5)
+    return result
+
+
+
+
